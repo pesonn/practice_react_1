8bbd9a4fe7c88b3c51c3dfe0aa59c18ccd5c09a2
pesonn <43933988+pesonn@users.noreply.github.com>
2020-02-02 17:19:37 +0100

    Navbar gestylet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,20 +3,22 @@ import React from "react";
 class NavBar extends React.Component {
   constructor() {
     super()
-    this.state = {}
+  
   }
 
   render() {
     return (
-      <nav className="navbar">
-        <img src="logo512.png" alt=""/>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>More</li>
-        </ul>
-      </nav>
+      <div className="navbar--wrapper">
+        <nav className="navbar">
+          <img src="logo512.png" alt=""/>
+          <ul className="navbar--navpoints">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">More</a></li>
+          </ul>
+        </nav>
+      </div>
     )
   }
 }
