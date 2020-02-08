a6c3f22e2de946aad3d100cfb226355a68074e95
pesonn <43933988+pesonn@users.noreply.github.com>
2020-02-09 00:55:10 +0100

    code funktioniert und aufgeräumt

--- src/components/NasaAPI.js
+++ src/components/NasaAPI.js
@@ -4,56 +4,40 @@ export default class NasaAPI extends React.Component {
   constructor(props) {
     super(props)
     this.state= {
-      items: {},
+      img: "",
       isLoaded: false,
-      url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=${props.camera}&api_key=DEMO_KEY`
     }
 
-    this.props= {
-      camera: props.camera
-    }
-  
-
+    this.loadAPI = this.loadAPI.bind(this)
   }
-  
-  async componentDidMount() {
-    const response = await fetch(this.state.url);
+
+  loadAPI = async () => {
+    const response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=${this.props.camera}&api_key=DEMO_KEY`);
     const data = await response.json();
     this.setState({
       isLoaded: true,
-      items: data.photos[Math.floor(Math.random() * 2)],
-      
-    })
-      
+      img: data.photos[Math.round(Math.random() * data.photos.length)].img_src,
+    }) 
   }
-
-  function changeUrl(selectedcamera) {
-    this.setState({url: `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=${selectedcamera}&api_key=DEMO_KEY`}) 
     
-  }; // TODO: Wie erstelle ich hier eine Funtion???
+  componentDidMount() {
+    this.loadAPI(this.props.camera)
+  }  
 
-  
-
-  componentDidUpdate() {
-    console.log("state " + this.props.camera);
-    changeUrl(this.props.camera)
-    console.log("state " + this.state.url);
-   
+   componentDidUpdate(prevProps) {
+   if(prevProps.camera !== this.props.camera) {
+    this.setState({isLoaded: false})
+    this.loadAPI(this.props.camera)
+   }
   }
-
-  
 
   render() {
-    const img = this.state.items.img_src
-    console.log(img)
-
     return(
       <div>
         {!this.state.isLoaded ? <p>Loading...</p> : null}
-        <img src={img} className="nasaimg"></img>
+        <img src={this.state.img} className="nasaimg"></img>
       </div>
     )
-    
   }
 }
 
